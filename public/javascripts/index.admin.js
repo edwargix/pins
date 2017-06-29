76e8b0ed763e5ef0b0d525bc62f2211e97e47e1c
David Florness <edwargix@gmail.com>
2017-06-28 19:35:44 -0500

    Create interface to move or delete a photo

--- public/javascripts/index.admin.js
+++ public/javascripts/index.admin.js
@@ -9,3 +9,30 @@ $('#add_location').on('click', function() {
     $('#mkdir').fadeIn();
   });
 });
+
+
+$('img').on('click', function() {
+  var img = this;
+  $('html, body').animate({
+    scrollTop: $('[id="' + img.id + '_title"]').offset().top
+  }, 400, function() {
+    window.location.hash = img.id + '_title';
+
+    $('[id="' + img.id + '_actions"]').fadeIn('fast', function() {
+
+      $('[id="' + img.id + '_cancel"]').off('click').on('click', function() {
+        $('[id="' + img.id + '_actions"]').fadeOut('fast');
+      });
+
+      $('[id="' + img.id + '_rename"]').off('click').on('click', function() {
+        $('[id="' + img.id + '_rename_form"]').toggle();
+      });
+
+      $('[id="' + img.id + '_delete"]').off('click').on('click', function() {
+        if (window.confirm('The pic will be deleted permanently'))
+          $.post('/rm', { path: $('div#path').html(), name: img.alt }, () => { window.location.reload(); });
+      });
+
+    });
+  });
+});
